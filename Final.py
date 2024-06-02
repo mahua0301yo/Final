ebe79e4e7a17c2d44f3ed774a564ad752f53422c
mahua0301yo <168515930+mahua0301yo@users.noreply.github.com>
2024-06-02 17:24:36 +0800

    Update Final.py

--- Final.py
+++ Final.py
@@ -134,7 +134,7 @@ def main():
 
     # 選擇資料區間
     st.subheader("選擇資料區間")
-    start_date = st.date_input('選擇開始日期', datetime.date(2000, 1, 1))
+    start_date = st.date_input('選擇開始日期', datetime.date(2015, 1, 1))
     end_date = st.date_input('選擇結束日期', datetime.date(2100, 12, 31))
     stockname = st.text_input('請輸入股票代號 (例: 2330.TW)', '2330.TW')
 
