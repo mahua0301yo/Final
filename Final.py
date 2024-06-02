a6a4873aea551d515aa11b7f82bac9d11570038c
mahua0301yo <168515930+mahua0301yo@users.noreply.github.com>
2024-06-02 17:15:01 +0800

    Update Final.py

--- Final.py
+++ Final.py
@@ -105,16 +105,7 @@ def plot_stock_data(stock, sma_period, ema_period):
 
     # 更新布局和樣式
     fig.update_layout(title='股票價格與技術指標',
-                      xaxis_rangeslider_visible=True,
-                     rangeselector=dict(
-                         buttons=list([
-                             dict(count=1, label="1m", step="month", stepmode="backward"),
-                             dict(count=6, label="6m", step="month", stepmode="backward"),
-                             dict(count=1, label="YTD", step="year", stepmode="todate"),
-                             dict(count=1, label="1y", step="year", stepmode="backward"),
-                             dict(step="all")
-                         ])
-                     ))
+                      xaxis_rangeslider_visible=True)
 
     # 調整布局
     fig.update_layout(
